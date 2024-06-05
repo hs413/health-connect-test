c6e27dbaf85b8f89c4e176a356378addf1d1ed39
rtg <rtg1539@naver.com>
2024-06-05 17:16:22 +0900

    운동 데이터

--- api/src/main/java/com/api/HealthApiController.java
+++ api/src/main/java/com/api/HealthApiController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
+
 @RestController
 public class HealthApiController {
 
     @PostMapping("/health")
-    public void health(@RequestBody Object s) {
-        System.out.println("a");
+    public void health(@RequestBody LinkedHashMap s) {
+        System.out.println(s.get("count"));
+        System.out.println(s.get("timestamp"));
     }
 }
--- health/app/src/main/java/com/example/health/health/HealthClient.kt
+++ health/app/src/main/java/com/example/health/health/HealthClient.kt
@@ -3,35 +3,33 @@ package com.example.health.health
 import android.content.Context
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.health.connect.client.HealthConnectClient
-import androidx.health.connect.client.records.ExerciseLap
+import androidx.health.connect.client.records.DistanceRecord
+import androidx.health.connect.client.records.ExerciseSegment
 import androidx.health.connect.client.records.ExerciseSessionRecord
-import androidx.health.connect.client.records.HeartRateRecord
-import androidx.health.connect.client.records.SpeedRecord
 import androidx.health.connect.client.records.StepsRecord
 import androidx.health.connect.client.request.ReadRecordsRequest
 import androidx.health.connect.client.time.TimeRangeFilter
+import androidx.health.connect.client.units.Length
 import com.example.health.api.RetrofitInstance
 import com.example.health.api.StepsData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import java.time.Instant
+import java.time.ZonedDateTime
+import java.util.TimeZone
 
 class HealthClient(private val context: Context, private val healthConnectClient: HealthConnectClient, private val webView: WebView/*, private val requestPermissions: ActivityResultLauncher<Set<String>>*/): WebViewClient() {
     suspend fun getRecords() {
         val endTime = Instant.now()
-        val startTime = endTime.minusSeconds(30 * 24 * 60 * 60) // 1주일 전
+        val startTime = endTime.minusSeconds(10 * 24 * 60 * 60)
         val response = healthConnectClient.readRecords(
             ReadRecordsRequest(
                 StepsRecord::class,
                 timeRangeFilter = TimeRangeFilter.between(startTime, endTime)
             )
         )
-        println("a")
+
         response.records.forEach {
             val steps = it.count
             println("AAAAAA" + steps);
@@ -50,9 +48,45 @@ class HealthClient(private val context: Context, private val healthConnectClient
         }
     }
     suspend fun insertSteps() {
+//        val endTime = Instant.now()
+//        val startTime = endTime.minusSeconds(1 * 60 * 60)
+
+        val endZoneId = TimeZone.getDefault().toZoneId();
         val endTime = Instant.now()
+        val endZonedDateTime = endTime.atZone(endZoneId);
+
+        val startZoneId = TimeZone.getDefault().toZoneId();
         val startTime = endTime.minusSeconds(1 * 60 * 60)
+        val zonedDateTime = startTime.atZone(startZoneId);
+
         try {
+            healthConnectClient.insertRecords(
+                listOf(
+                    ExerciseSessionRecord(
+                        startTime = zonedDateTime.toInstant(),
+                        startZoneOffset = zonedDateTime.offset,
+                        endTime = endZonedDateTime.toInstant(),
+                        endZoneOffset = endZonedDateTime.offset,
+                        exerciseType = ExerciseSessionRecord.EXERCISE_TYPE_SWIMMING_POOL,
+                        title = "My Swim",
+                        segments = listOf(
+                            ExerciseSegment(
+                                segmentType = ExerciseSegment.EXERCISE_SEGMENT_TYPE_SWIMMING_BACKSTROKE,
+                                startTime = zonedDateTime.toInstant(),
+                                endTime = endZonedDateTime.toInstant(),
+                                repetitions = 8,
+                            )
+                        ),
+                    ),
+                ) + buildHeartRateSeries(zonedDateTime, endZonedDateTime)
+            )
+
+
+        } catch (e: Exception) {
+            //
+        }
+
+       /* try {
             val stepsRecord = StepsRecord(
                 count = 120,
                 startTime = startTime,
@@ -63,12 +97,25 @@ class HealthClient(private val context: Context, private val healthConnectClient
             healthConnectClient.insertRecords(listOf(stepsRecord))
         } catch (e: Exception) {
             // Run error handling here
-        }
+        }*/
+    }
+
+    private fun buildHeartRateSeries(
+        sessionStartTime: ZonedDateTime,
+        sessionEndTime: ZonedDateTime,
+    ): DistanceRecord {
+        return DistanceRecord(
+            distance = Length.meters(1100.1),
+            startTime = sessionStartTime.toInstant(),
+            startZoneOffset = sessionStartTime.offset,
+            endTime = sessionEndTime.toInstant(),
+            endZoneOffset = sessionEndTime.offset,
+        )
     }
 
     suspend fun getExercise() {
         val endTime = Instant.now()
-        val startTime = endTime.minusSeconds(12 * 60 * 60) // 1주일 전
+        val startTime = endTime.minusSeconds( 10 * 24 * 60 * 60) // 10일 전
         val response =
             healthConnectClient.readRecords(
                 ReadRecordsRequest(
@@ -84,7 +131,8 @@ class HealthClient(private val context: Context, private val healthConnectClient
             val heartRateRecords =
                 healthConnectClient
                     .readRecords(
-                        ReadRecordsRequest<SpeedRecord>(
+                        ReadRecordsRequest(
+                            DistanceRecord::class,
                             timeRangeFilter =
                             TimeRangeFilter.between(
                                 exerciseRecord.startTime,
@@ -93,6 +141,7 @@ class HealthClient(private val context: Context, private val healthConnectClient
                         )
                     )
                     .records
+            println(heartRateRecords)
         }
 
        /* withContext(Dispatchers.Main) {
